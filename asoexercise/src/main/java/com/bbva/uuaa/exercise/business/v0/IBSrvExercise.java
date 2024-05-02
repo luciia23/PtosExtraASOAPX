dfdb9dd37d0805baf514c2deee88fff0bdab2cc1
luciia23 <l.colladomartinrq@gmail.com>
2024-05-02 16:01:13 +0200

    aso extra exercise added

--- asoexercise/src/main/java/com/bbva/uuaa/exercise/business/v0/IBSrvExercise.java
+++
@@ -1,8 +0,0 @@
-package com.bbva.uuaa.exercise.business.v0;
-
-import com.bbva.uuaa.exercise.business.v0.dto.BUser;
-import com.bbva.uuaa.exercise.business.v0.dto.BUserOut;
-
-public interface IBSrvExercise {
-    BUserOut mapBUserOut(BUser bUser);
-}
--- asoexercise/src/main/java/com/bbva/uuaa/exercise/business/v0/dao/ISrvExerciseDAO.java
+++
@@ -1,8 +0,0 @@
-package com.bbva.uuaa.exercise.business.v0.dao;
-
-import com.bbva.uuaa.exercise.business.v0.dto.BUser;
-import com.bbva.uuaa.exercise.business.v0.dto.BUserOut;
-
-public interface ISrvExerciseDAO {
-    BUserOut mapBUserOut(BUser bUser);
-}
